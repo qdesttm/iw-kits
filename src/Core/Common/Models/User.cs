75f66faca3b5bd12cfe1a281ac112be36cd76311
QDestTM <dimag4681@gmail.com>
2026-09-21 01:55:06 +0300

    Add user model

--- src/Core/Common/Models/User.cs
+++ src/Core/Common/Models/User.cs
@@ -3,10 +3,10 @@ using System;
 namespace IWKits.Core.Common.Models;
 
 /// <summary>
-/// 
+/// Represents a user within the application domain.
 /// </summary>
-/// <param name="Id"></param>
-/// <param name="Username"></param>
-/// <param name="PasswordHash"></param>
-/// <param name="Role"></param>
+/// <param name="Id">The unique identifier of the user.</param>
+/// <param name="Username">The unique username used for authentication.</param>
+/// <param name="PasswordHash">The secure cryptographic hash of the user's password.</param>
+/// <param name="Role">The system or domain role assigned to the user.</param>
 public sealed record User(Guid Id, string Username, string PasswordHash, string Role);
