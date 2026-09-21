9fa46ae0df1b21e927603c54f74ad61d499ad1c2
QDestTM <dimag4681@gmail.com>
2026-09-21 23:36:39 +0300

    Update entry point

--- src/Api/AspNetCore/Program.cs
+++ src/Api/AspNetCore/Program.cs
@@ -1,4 +1,6 @@
+using FluentValidation;
 using IWKits.Api.AspNetCore.Hosting;
+using IWKits.Api.AspNetCore.Infrastructure;
 using IWKits.Core.Application.DependencyInjection;
 using IWKits.Core.Application.Options;
 using IWKits.Core.Data.DependencyInjection;
@@ -16,13 +18,30 @@ services.Configure<SecurityTokensOptions>(configuration.GetSection(SecurityToken
 services.Configure<MongodbOptions>(configuration.GetSection(MongodbOptions.SectionName));
 services.Configure<CacheOptions>(configuration.GetSection(CacheOptions.SectionName));
 
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+services.AddExceptionHandler<ServiceExceptionHandler>();
+services.AddProblemDetails();
+
 services.AddMediatR(options =>
-	options.RegisterServicesFromAssembly(typeof(Program).Assembly));
+    options.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+services.AddValidatorsFromAssemblyContaining<Program>();
+
+services.AddAuthentication().AddJwtBearer();
+services.AddAuthorization();
 
 services.AddDbContext();
 services.AddApplication();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapEndpoints();
 
