d7b9ce1dca92fb8f31419db31745f1592fe57aee
QDestTM <dimag4681@gmail.com>
2026-09-21 15:10:20 +0300

    Add mapper to convert TaxRate to OrderTaxBreakdown

--- src/Core/Application/Mappers/TaxRateMapper.cs
+++ src/Core/Application/Mappers/TaxRateMapper.cs
@@ -9,4 +9,12 @@ internal static partial class TaxRateMapper
 {
 	[MapperIgnoreSource(nameof(TaxRateEntity.Id))]
 	public static partial TaxRate ToTaxRate(this TaxRateEntity entity);
+
+	[MapProperty(nameof(TaxRate.EstimatedSpecialRate), nameof(OrderTaxBreakdown.SpecialRate))]
+	[MapProperty(nameof(TaxRate.EstimatedCountyRate), nameof(OrderTaxBreakdown.CountyRate))]
+	[MapProperty(nameof(TaxRate.EstimatedCityRate), nameof(OrderTaxBreakdown.CityRate))]
+	[MapperIgnoreSource(nameof(TaxRate.EstimatedCombinedRate))]
+	[MapperIgnoreSource(nameof(TaxRate.ZipCode))]
+	[MapperIgnoreSource(nameof(TaxRate.StateId))]
+	public static partial OrderTaxBreakdown ToOrderTaxBreakdown(this TaxRate taxRate);
 }
